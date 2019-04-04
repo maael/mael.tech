bd2039c2da118098e21d45ff9ce69ae85b1a72ac
Matthew Elphick <matthew.elphick@threadsstyling.com>
2019-04-04 21:54:28 +0100

    feat: 🎸 Initial layout etc

---
+++ components/SweetAndSaltyBg.js
@@ -0,0 +1,96 @@
+import {randomBetween, randomBetweenFloat} from '../lib/util';
+
+export const colours = [
+  {
+    r: 209,
+    g: 53,
+    b: 144
+  },
+  {
+    r: 240,
+    g: 126,
+    b: 62
+  },
+  {
+    r: 105,
+    g: 201,
+    b: 209
+  },
+  {
+    r: 186,
+    g: 171,
+    b: 89
+  },
+  {
+    r: 229,
+    g: 34,
+    b: 83
+  }
+]
+
+function firstDraw (c) {
+  if (!c) return;
+  c.width = window.innerWidth;
+  const ctx = c.getContext("2d");
+  ctx.globalCompositeOperation = "xor";
+
+  let circles = [];
+
+
+
+  function createNewCircle() {
+    if (circles.length > 3) return
+    if (randomBetween(0, 50) > 1) return;
+    const maxWidth = randomBetween(5, 15);
+    circles.push({
+      id: circles.length,
+      x: randomBetween(0, c.width),
+      y: randomBetween(0, c.height),
+      radius: 0,
+      max: randomBetween(Math.min(c.height / 10, c.width / 10), Math.max(c.height / 4, c.width / 4)),
+      speed: randomBetweenFloat(0.5, 1.2),
+      widthMax: maxWidth,
+      width: maxWidth,
+      opacity: 1,
+      color: colours[randomBetween(0, colours.length)]
+    })
+  }
+
+  function draw() {
+    createNewCircle();
+    circles = circles.filter((c) => c.radius <= c.max);
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.fillStyle = "#ffffff";
+    ctx.font = '64px LeagueGothic';
+    ctx.fillText('Matthew Elphick', 50, 100);
+    ctx.font = '48px LeagueGothic';
+    ctx.fillText('Software Engineer', 50, 175);
+    [...circles].forEach((circle) => {
+      ctx.save();
+      circle.radius += circle.speed;
+      if (circle.radius > circle.max - (circle.widthMax * 2)) {
+         circle.width -= circle.speed / 2;
+        if (circle.width < 0) circle.width = 0;
+         circle.opacity -= 0.01;
+        if (circle.opacity < 0) circle.opacity = 0;
+      }
+      ctx.beginPath();
+      ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+      ctx.lineWidth = circle.width;
+      ctx.strokeStyle = `rgba(${circle.color.r},${circle.color.g},${circle.color.b},${circle.opacity})`
+      ctx.stroke();
+      ctx.restore();
+    })
+
+    window.requestAnimationFrame(draw);
+  }
+
+  window.requestAnimationFrame(draw);
+}
+
+export default () => {
+  return <canvas height={300} ref={async (ref) => {
+    await (new FontFace('LeagueGothic', 'url(/static/fonts/leaguegothic-regular-webfont.ttf)')).load();
+    firstDraw(ref)
+  }}></canvas>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,72 @@
+import Head from 'next/head';
+import {TiSocialGithubCircular, TiSocialLinkedinCircular} from 'react-icons/ti';
+import Bg, {colours} from '../components/SweetAndSaltyBg';
+import {colourObjectToString, randomBetween} from '../lib/util';
+
+const styles = {
+  box: {
+    padding: '20px 100px',
+    boxSizing: 'border-box',
+    width: '100vw',
+    flex: 1,
+    color: '#ffffff',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  innerBox: {
+    padding: '20px 0px',
+  }
+}
+
 export default () => (
-  <h1>mael.tech</h1>
+  <div style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=0.8"/>
+    </Head>
+    <style jsx global>{`
+      @font-face {
+        font-family: 'LeagueGothic';
+        src: url('/static/fonts/leaguegothic-regular-webfont.eot');
+        src: url('/static/fonts/leaguegothic-regular-webfont.eot?#iefix') format('embedded-opentype'),
+             url('/static/fonts/leaguegothic-regular-webfont.woff') format('woff'),
+             url('/static/fonts/leaguegothic-regular-webfont.ttf') format('truetype'),
+             url('/static/fonts/leaguegothic-regular-webfont.svg#league_gothicregular') format('svg');
+        font-weight: normal;
+        font-style: normal;
+      }
+      * {
+        padding: 0;
+        margin: 0;
+      }
+      body {
+        background: #21272D;
+        font-family: Arial, sans-serif;
+      }
+      a {
+        color: #FFFFFF;
+        text-decoration: none;
+      }
+    `}</style>
+    <div style={{position: 'relative', height: 300}}>
+      <Bg />
+      <div style={{fontFamily: 'LeagueGothic', fontSize: 64, position: 'absolute', top: 40, left: 50, opacity: 0}}>Matthew Elphick</div>
+      <div style={{fontFamily: 'LeagueGothic', fontSize: 48, position: 'absolute', top: 130, left: 50, opacity: 0}}>Software Engineer</div>
+    </div>
+    <div style={{...styles.box, background: colourObjectToString(colours[randomBetween(0, colours.length)]), color: '#000000'}}>
+      <div style={styles.innerBox}>Hi, I'm Matt. I'm a Software Engineer, and I work at <a href='https://threadsstyling.com'>Threads Styling</a>.</div>
+      <div style={styles.innerBox}>You can find work I've been doing on my <a href='https://github.com/maael'>GitHub</a> profile.</div>
+      <div style={styles.innerBox}>I mostly work with JavaScript, but I have an interest in all things web.</div>
+    </div>
+    <div style={{...styles.box, alignItems: 'center'}}>
+      <div>
+        <a href='https://github.com/maael'>
+          <TiSocialGithubCircular size={50} />
+        </a>
+        <a href='https://www.linkedin.com/in/melphick/'>
+          <TiSocialLinkedinCircular size={50} />
+        </a>
+      </div>
+    </div>
+  </div>
 );
